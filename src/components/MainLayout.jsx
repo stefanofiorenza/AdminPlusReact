b3cff910072d3f12df2f5e87ad8adc54762bd62b
stefanofiorenza <stefanofiorenza@gmail.com>
2020-11-05 14:18:06 +0200

    change class into className

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -14,42 +14,42 @@ function MainLayout() {
    
   return (
    
-   <div classname="container-fluid fixed">
+   <div className="container-fluid ">
 				
         {/* Navigation Bar Start */ }
-        <div class="navbar main">
-			    <a href="index.html?lang=en" class="appbrand"><span>Admin+ <span>lovely headline here</span></span></a>
+        <div className="navbar main">
+			    <a href="index.html?lang=en" className="appbrand"><span>Admin+ <span>lovely headline here</span></span></a>
 			
-						<button type="button" class="btn btn-navbar">
-				    <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+						<button type="button" className="btn btn-navbar">
+				    <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span>
 			    </button>
 						
-			<ul class="topnav pull-right">
-				<li class="visible-desktop">
-					<ul class="notif">
-						<li><a href="" class="glyphicons envelope" data-toggle="tooltip" data-placement="bottom" data-original-title="5 new messages"><i></i> 5</a></li>
-						<li><a href="" class="glyphicons shopping_cart" data-toggle="tooltip" data-placement="bottom" data-original-title="1 new orders"><i></i> 1</a></li>
-						<li><a href="" class="glyphicons log_book" data-toggle="tooltip" data-placement="bottom" data-original-title="3 new activities"><i></i> 3</a></li>
+			<ul className="topnav pull-right">
+				<li className="visible-desktop">
+					<ul className="notif">
+						<li><a href="" className="glyphicons envelope" data-toggle="tooltip" data-placement="bottom" data-original-title="5 new messages"><i></i> 5</a></li>
+						<li><a href="" className="glyphicons shopping_cart" data-toggle="tooltip" data-placement="bottom" data-original-title="1 new orders"><i></i> 1</a></li>
+						<li><a href="" className="glyphicons log_book" data-toggle="tooltip" data-placement="bottom" data-original-title="3 new activities"><i></i> 3</a></li>
 					</ul>
 				</li>
-				<li class="dropdown visible-desktop">
-					<a href="" data-toggle="dropdown" class="glyphicons cogwheel"><i></i>Dropdown <span class="caret"></span></a>
-					<ul class="dropdown-menu pull-right">
+				<li className="dropdown visible-desktop">
+					<a href="" data-toggle="dropdown" className="glyphicons cogwheel"><i></i>Dropdown <span className="caret"></span></a>
+					<ul className="dropdown-menu pull-right">
 						<li><a href="">Some option</a></li>
 						<li><a href="">Some other option</a></li>
 						<li><a href="">Other option</a></li>
 					</ul>
 				</li>
-				<li class="account">
-					<a data-toggle="dropdown" href="form_demo.html?lang=en" class="glyphicons logout lock">
-						<span class="hidden-phone text">mosaicpro</span><i></i>
+				<li className="account">
+					<a data-toggle="dropdown" href="form_demo.html?lang=en" className="glyphicons logout lock">
+						<span className="hidden-phone text">mosaicpro</span><i></i>
 					</a>
 					
-					<ul class="dropdown-menu pull-right">
-						<li><a href="form_demo.html?lang=en" class="glyphicons cogwheel">Settings<i></i></a></li>
+					<ul className="dropdown-menu pull-right">
+						<li><a href="form_demo.html?lang=en" className="glyphicons cogwheel">Settings<i></i></a></li>
 						<li>
 							<span>
-								<a class="btn btn-default btn-small pull-right" style={dropDownLocalStyle} href="login.html?lang=en">Sign Out</a>
+								<a className="btn btn-default btn-small pull-right" style={dropDownLocalStyle} href="login.html?lang=en">Sign Out</a>
 							</span>
 						</li>
 					</ul>
@@ -64,26 +64,26 @@ function MainLayout() {
 	{/*<!-- Start Wrapper  -->  */ }
   <div id="wrapper">
 
-			<div id="menu" class="hidden-phone">
+			<div id="menu" className="hidden-phone">
 				<div id="menuInner">
 					<div id="search">
 						<input type="text" placeholder="Quick search ..." />
-						<button class="glyphicons search"><i></i></button>
+						<button className="glyphicons search"><i></i></button>
 					</div>
 					
 					<ul>
-						<li class="heading"><span>E Shop</span></li>
-						<li class="glyphicons search"><a href="finances.html?lang=en"><i></i><span>Search Products</span></a></li>
+						<li className="heading"><span>E Shop</span></li>
+						<li className="glyphicons search"><a href="finances.html?lang=en"><i></i><span>Search Products</span></a></li>
 					</ul>
 					<ul>
-						<li class="heading"><span>Users</span></li>
-						<li class="glyphicons user_add"><a href="finances.html?lang=en"><i></i><span>Create User</span></a></li>
-						<li class="glyphicons group"><a href="finances.html?lang=en"><i></i><span>Admin Users</span></a></li>		
+						<li className="heading"><span>Users</span></li>
+						<li className="glyphicons user_add"><a href="finances.html?lang=en"><i></i><span>Create User</span></a></li>
+						<li className="glyphicons group"><a href="finances.html?lang=en"><i></i><span>Admin Users</span></a></li>		
 					</ul>				
 					<ul>
-						<li class="heading"><span>Admin</span></li>					
-						<li class="glyphicons gift"><a href="finances.html?lang=en"><i></i><span>Create Product</span></a></li>	
-						<li class="glyphicons table"><a href="finances.html?lang=en"><i></i><span>Admin Products</span></a></li>									
+						<li className="heading"><span>Admin</span></li>					
+						<li className="glyphicons gift"><a href="finances.html?lang=en"><i></i><span>Create Product</span></a></li>	
+						<li className="glyphicons table"><a href="finances.html?lang=en"><i></i><span>Admin Products</span></a></li>									
 					</ul>
 				</div>
 		</div>
@@ -93,83 +93,83 @@ function MainLayout() {
 			
 
 
-		<div class="innerLR">
+		<div className="innerLR">
 				
-			<div class="row-fluid">							
-				<div class="span12">
-					<div class="widget-head">
-						<div class="heading-buttons">
-							<h3 class="glyphicons gift"><i></i>Products</h3>					
-							<div class="clearfix"></div>
+			<div className="row-fluid">							
+				<div className="span12">
+					<div className="widget-head">
+						<div className="heading-buttons">
+							<h3 className="glyphicons gift"><i></i>Products</h3>					
+							<div className="clearfix"></div>
 						</div>
 					</div>
-				<hr class="separator bottom" />
-				<div class="widget-body" style={{ padding: '10px' }}>
+				<hr className="separator bottom" />
+				<div className="widget-body" style={{ padding: '10px' }}>
 				{/*<!-- --------------------------------------------- -->	*/}
 					
-				<table class="table table-bordered table-condensed table-striped table-primary table-vertical-center checkboxs">
+				<table className="table table-bordered table-condensed table-striped table-primary table-vertical-center checkboxs">
 					<thead>
 						<tr>
-							<th class="center">No.</th>
-							<th class="center">Time</th>
+							<th className="center">No.</th>
+							<th className="center">Time</th>
 							<th>Client</th>
-							<th class="center">Phone</th>
-							<th class="center">Amount</th>
-							<th class="center">Heard from</th>
-							<th class="center" style={{ width: '90px' }} >Actions</th>
+							<th className="center">Phone</th>
+							<th className="center">Amount</th>
+							<th className="center">Heard from</th>
+							<th className="center" style={{ width: '90px' }} >Actions</th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr class="selectable">
-							<td class="center">1</td>
-							<td class="center">11:00</td>
+						<tr className="selectable">
+							<td className="center">1</td>
+							<td className="center">11:00</td>
 							<td><strong>Lorem Ipsum</strong><br/><small>Location: Miami</small></td>
-							<td class="center">0740000000</td>
-							<td class="center">#30</td>
-							<td class="center">Friend</td>
-							<td class="center">
-								<a href="#" class="btn-action glyphicons eye_open btn-info"><i></i></a>
-								<a href="#" class="btn-action glyphicons pencil btn-success"><i></i></a>
-								<a href="#" class="btn-action glyphicons remove_2 btn-danger"><i></i></a>
+							<td className="center">0740000000</td>
+							<td className="center">#30</td>
+							<td className="center">Friend</td>
+							<td className="center">
+								<a href="#" className="btn-action glyphicons eye_open btn-info"><i></i></a>
+								<a href="#" className="btn-action glyphicons pencil btn-success"><i></i></a>
+								<a href="#" className="btn-action glyphicons remove_2 btn-danger"><i></i></a>
 							</td>
 						</tr>
-						<tr class="selectable selected">
-							<td class="center">2</td>
-							<td class="center">12:00</td>
+						<tr className="selectable selected">
+							<td className="center">2</td>
+							<td className="center">12:00</td>
 							<td><strong>Lorem Ipsum</strong><br/><small>Location: Hollywood</small></td>
-							<td class="center">0740000000</td>
-							<td class="center">#39</td>
-							<td class="center">Friend</td>
-							<td class="center">
-								<a href="#" class="btn-action glyphicons eye_open btn-info"><i></i></a>
-								<a href="#" class="btn-action glyphicons pencil btn-success"><i></i></a>
-								<a href="#" class="btn-action glyphicons remove_2 btn-danger"><i></i></a>
+							<td className="center">0740000000</td>
+							<td className="center">#39</td>
+							<td className="center">Friend</td>
+							<td className="center">
+								<a href="#" className="btn-action glyphicons eye_open btn-info"><i></i></a>
+								<a href="#" className="btn-action glyphicons pencil btn-success"><i></i></a>
+								<a href="#" className="btn-action glyphicons remove_2 btn-danger"><i></i></a>
 							</td>
 						</tr>
-						<tr class="selectable">
-							<td class="center">3</td>
-							<td class="center">13:00</td>
+						<tr className="selectable">
+							<td className="center">3</td>
+							<td className="center">13:00</td>
 							<td><strong>Lorem Ipsum</strong><br/><small>Location: Miami</small></td>
-							<td class="center">0740000000</td>
-							<td class="center">31</td>
-							<td class="center">Google Search</td>
-							<td class="center">
-								<a href="#" class="btn-action glyphicons eye_open btn-info"><i></i></a>
-								<a href="#" class="btn-action glyphicons pencil btn-success"><i></i></a>
-								<a href="#" class="btn-action glyphicons remove_2 btn-danger"><i></i></a>
+							<td className="center">0740000000</td>
+							<td className="center">31</td>
+							<td className="center">Google Search</td>
+							<td className="center">
+								<a href="#" className="btn-action glyphicons eye_open btn-info"><i></i></a>
+								<a href="#" className="btn-action glyphicons pencil btn-success"><i></i></a>
+								<a href="#" className="btn-action glyphicons remove_2 btn-danger"><i></i></a>
 							</td>
 						</tr>
-						<tr class="selectable">
-							<td class="center">4</td>
-							<td class="center">14:00</td>
+						<tr className="selectable">
+							<td className="center">4</td>
+							<td className="center">14:00</td>
 							<td><strong>Lorem Ipsum</strong><br/><small>Location: Miami</small></td>
-							<td class="center">0740000000</td>
-							<td class="center">43</td>
-							<td class="center">Google Search</td>
-							<td class="center">
-								<a href="#" class="btn-action glyphicons eye_open btn-info"><i></i></a>
-								<a href="#" class="btn-action glyphicons pencil btn-success"><i></i></a>
-								<a href="#" class="btn-action glyphicons remove_2 btn-danger"><i></i></a>
+							<td className="center">0740000000</td>
+							<td className="center">43</td>
+							<td className="center">Google Search</td>
+							<td className="center">
+								<a href="#" className="btn-action glyphicons eye_open btn-info"><i></i></a>
+								<a href="#" className="btn-action glyphicons pencil btn-success"><i></i></a>
+								<a href="#" className="btn-action glyphicons remove_2 btn-danger"><i></i></a>
 							</td>
 						</tr>
 					</tbody>
